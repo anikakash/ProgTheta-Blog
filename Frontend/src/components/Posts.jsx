cc485f8a180fd22b8f1dd9580d3de2d77180b95a
anikakash <anikdash989@gmail.com>
2024-04-22 08:32:22 +0600

    small bug fix

--- Frontend/src/components/Posts.jsx
+++ Frontend/src/components/Posts.jsx
@@ -1,56 +1,20 @@
 import React, { useState } from 'react';
-import thumbnail11 from '../assets/blog1.jpg';
-import thumbnail22 from '../assets/blog2.jpg';
-import thumbnail33 from '../assets/blog3.jpg';
-import { default as thumbnail44 } from '../assets/blog4.jpg';
+
+import { DUMMY_POSTS } from '../data';
 import PostItem from './PostItem';
-const DUMMY_POSTS = [
-    {
-      id: "1",
-      thumbnail: thumbnail11,
-      category: "system-design",
-      title: "Introduction to System Design",
-      desc: "Learn the basics of system design and architecture. Understand key concepts such as scalability, reliability, and performance. Explore various design patterns and best practices. Dive into real-world case studies and examples. Gain practical skills for designing scalable and efficient systems.Dive into back-end technologies like Node.js and Django. Discover best practices for building responsive and user-friendly web applications. Gain hands-on experience through projects and exercises",
-      authorID: 3
-    },
-    {
-      id: "2",
-      thumbnail: thumbnail22,
-      category: "web-development",
-      title: "Building Modern Web Applications",
-      desc: "Explore the latest tools and technologies for web development. Learn about front-end frameworks such as React and Vue.js. Dive into back-end technologies like Node.js and Django. Discover best practices for building responsive and user-friendly web applications. Gain hands-on experience through projects and exercises.",
-      authorID: 5
-    },
-    {
-      id: "3",
-      thumbnail: thumbnail33,
-      category: "data-science",
-      title: "Machine Learning Fundamentals",
-      desc: "Discover the essential concepts of machine learning. Explore algorithms for regression, classification, and clustering. Learn how to preprocess data and evaluate model performance. Dive into advanced topics like deep learning and natural language processing. Gain practical experience through projects and case studies.",
-      authorID: 2
-    },
-    {
-      id: "4",
-      thumbnail: thumbnail44,
-      category: "cloud-computing",
-      title: "Introduction to Cloud Computing",
-      desc: "Understand the fundamentals of cloud computing and its applications. Learn about different cloud service models: Infrastructure as a Service (IaaS), Platform as a Service (PaaS), and Software as a Service (SaaS). Explore popular cloud providers like AWS, Azure, and Google Cloud. Gain hands-on experience with cloud services and deployment techniques.",
-      authorID: 4
-    }
-];  
 
 const Posts = () => {
     const [posts, setPosts] = useState(DUMMY_POSTS);
     
     return (
         <section className="posts">
-          <div className="container posts__container">
+          {posts.length > 0 ? <div className="container posts__container">
               {posts.map(({ id, thumbnail, category, title, desc, authorID }) => (
                   <div key={id} className="post">
                       <PostItem postID={id} thumbnail={thumbnail} category={category} title={title} description={desc} authorID={authorID} />
                   </div>
-              ))}
-          </div>
+              )) }
+          </div> : <h2 className='center'>No posts founds</h2>}
         </section>
     );
 };
--- Frontend/src/pages/PostDetails.jsx
+++ Frontend/src/pages/PostDetails.jsx
@@ -1,5 +1,6 @@
 import React from 'react'
 import { Link } from 'react-router-dom'
+import Thumbnail from '../assets/blog1.jpg'
 import PostAuthor from '../components/PostAuthor'
 
 export const PostDetails = () => {
@@ -14,6 +15,19 @@ export const PostDetails = () => {
             </div>
           </div>
           <h1>Ths is the post titel</h1>
+          <div className="post-detail__thumbnail">
+              <img src={Thumbnail} alt="" />
+          </div>
+          <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Excepturi, pariatur cupiditate. Ipsum aut corrupti illum deleniti facere, aspernatur corporis ipsa obcaecati eum sapiente reprehenderit quia! Explicabo tempore aperiam quas expedita maxime qui provident, et iure debitis reiciendis. Earum, officia voluptas.
+          </p>
+          <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Iusto, at dolorem quos fugiat quas recusandae maxime pariatur ea praesentium autem nobis alias voluptatibus deserunt cum eos nemo? Eum autem unde iusto quas placeat iure nostrum. Eos amet laboriosam totam eum reiciendis, voluptatibus nulla odit voluptatem rem cum adipisci omnis sunt nisi possimus, eius magni culpa.</p>
+          <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Ipsam temporibus totam pariatur est assumenda, blanditiis ullam alias dolorem porro molestias vero expedita voluptatibus ipsa laudantium mollitia eos fugiat soluta iure! Recusandae non itaque assumenda. Aspernatur fuga labore nobis reprehenderit quos molestias amet asperiores eius doloribus, quam, optio repudiandae voluptatum ipsa natus fugiat culpa voluptate, sunt a. Soluta nemo natus architecto! At consequatur, officiis nobis, iusto ducimus amet fuga molestiae architecto blanditiis voluptatum dolor placeat? Laboriosam voluptate nihil quis sequi minus dolorem voluptatibus sed, consequuntur excepturi fugiat esse, voluptates, perspiciatis doloremque aliquid ipsam libero earum voluptatum atque? Debitis earum pariatur nesciunt eum magnam in cumque suscipit vero! Expedita possimus tempora nemo minima.</p>
+
+          <p>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Deleniti, possimus soluta. 
+
+          </p>
+          <p><br />Lorem ipsum dolor, sit amet consectetur adipisicing elit. Aliquid et quod inventore autem nisi nemo obcaecati alias odio impedit saepe. Odit cumque a at eum quasi numquam sapiente consequuntur quibusdam quo beatae laborum nostrum iure sunt ab, quos iste voluptates autem placeat minus explicabo temporibus. Illo quibusdam nemo dignissimos inventore iure quod magnam impedit et a doloribus dolorem atque explicabo expedita quasi repellat sed laudantium qui veniam deserunt fuga, consectetur officiis iusto! Magnam totam consectetur libero tempore assumenda possimus minima, aliquam molestiae illo doloremque! Amet minima tempore quasi veritatis nobis necessitatibus excepturi quaerat. Soluta sed a consectetur voluptate similique beatae nesciunt cupiditate numquam commodi unde ipsam, architecto quos consequatur nulla necessitatibus inventore molestiae harum minus aperiam asperiores? Repellat sequi minima quod corrupti, voluptatibus harum totam dolor accusamus dolore vel iusto tenetur fuga quisquam culpa soluta, incidunt laudantium ea reprehenderit consequuntur. Quam distinctio molestias illo natus reprehenderit amet sint iusto ullam non, voluptatem accusantium molestiae quis itaque excepturi, esse explicabo. Neque deserunt repudiandae, dolorem labore suscipit aperiam vitae quaerat debitis et id accusantium fuga sint? Dolorum et porro debitis accusantium enim iure possimus dolorem distinctio, culpa unde odit, ratione incidunt deleniti beatae amet doloribus vel iusto. Facere sequi obcaecati necessitatibus porro!</p>
       </div>
     </section>
   )
